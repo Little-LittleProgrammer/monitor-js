1e825d0ab3f305f35bd44cc87099ae56f583ae51
Evan Wu <wuanle@qimao.com>
2022-08-05 10:54:26 +0800

    fix: 回滚上报数据缓存记录到内存

--- packages/utils/src/queue.ts
+++ packages/utils/src/queue.ts
@@ -1,28 +1,16 @@
-import { isWx } from './is';
-import { createLocalStorage, createWxStorage } from './storage';
 import { deep_copy } from './tools';
 export class Queue {
-    private storage:any;
+    private stack: any[];
     constructor() {
-        if (isWx) {
-            this.storage = createWxStorage({
-                hasEncrypt: true
-            });
-        } else {
-            this.storage = createLocalStorage({
-                hasEncrypt: true
-            });
-        }
+        this.stack = [];
     }
     get_cache() {
-        return deep_copy(this.storage.get('qMonitorCache'));
+        return deep_copy(this.stack);
     }
     add_cache(data: any) {
-        const _data = this.storage.get('qMonitorCache') || [];
-        _data.push(data);
-        this.storage.set('qMonitorCache', _data);
+        this.stack.push(data);
     }
     clear_cache() {
-        this.storage.remove('qMonitorCache');
+        this.stack = [];
     }
 }
