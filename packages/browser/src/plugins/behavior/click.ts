61e9bf8d0eee233afab956abceb83ea5a19c6ba2
Evan Wu <wuanle@qimao.com>
2022-08-11 11:45:49 +0800

    fix: 更改 click 的dom监测方式

--- packages/browser/src/plugins/behavior/click.ts
+++ packages/browser/src/plugins/behavior/click.ts
@@ -4,6 +4,13 @@ import { BasePluginType } from '@qmonitor/types';
 import { get_page_url, html_element_to_string, on, throttle_event, _global } from '@qmonitor/utils';
 import { ReportBehaviorData } from '../../types';
 
+export interface DomCollectedType {
+    // maybe will add doubleClick or other in the future
+    category: 'click'
+    data: Document,
+    e: Event
+  }
+
 const clickPlugin: BasePluginType<BrowserBehaviorTypes, BrowserClient> = {
     name: BrowserBehaviorTypes.CLICK,
     type: MonitorClassTypes.behavior,
@@ -11,37 +18,57 @@ const clickPlugin: BasePluginType<BrowserBehaviorTypes, BrowserClient> = {
         if (!('document' in _global)) return;
         // 电脑端click
         on(_global.document, BrowserEventTypes.CLICK, function(e: Event) {
+            const _this = this;
             throttle_event(notify, {
-                args: [BrowserBehaviorTypes.CLICK, e]
+                args: [BrowserBehaviorTypes.CLICK, {
+                    category: 'click',
+                    data: _this,
+                    e
+                }]
             });
         }, true);
         // 移动端touch
         on(_global.document, BrowserEventTypes.TOUCHSTART, function(e: Event) {
+            const _this = this;
             throttle_event(notify, {
-                args: [BrowserBehaviorTypes.CLICK, e]
+                args: [BrowserBehaviorTypes.CLICK, {
+                    category: 'click',
+                    data: _this,
+                    e
+                }]
             });
         }, true);
     },
-    transform(e: Event) {
-        const _htmlString = html_element_to_string(e.target as HTMLElement);
-        const _area = (e.target as any)?.getBoundingClientRect();
-        const _reportData: ReportBehaviorData = {
-            type: MonitorClassTypes.behavior,
-            subType: BrowserBehaviorTypes.CLICK,
-            pageURL: get_page_url(),
-            extraData: {
-                startTime: e.timeStamp,
-                district: {
-                    top: _area?.top,
-                    left: _area?.left
-                },
-                html: _htmlString
+    transform(collectedData: DomCollectedType) {
+        const { data, e } = collectedData;
+        const _htmlString = html_element_to_string(data.activeElement as HTMLElement);
+        if (_htmlString) { // 如果是body 则不上报
+            const _reportData: ReportBehaviorData = {
+                type: MonitorClassTypes.behavior,
+                subType: BrowserBehaviorTypes.CLICK,
+                pageURL: get_page_url(),
+                extraData: {
+                    startTime: e.timeStamp,
+                    district: {
+                    },
+                    html: _htmlString
+                }
+            };
+            const _area = (e.target as any).getBoundingClientRect();
+            if (_area) {
+                _reportData.extraData.district = {
+                    top: _area.top,
+                    left: _area.left
+                };
             }
-        };
-        return _reportData;
+            return _reportData;
+        }
+        return null;
     },
     consumer(reportData: ReportBehaviorData) {
-        this.report.send(reportData);
+        if (reportData) {
+            this.report.send(reportData);
+        }
     }
 };
 
