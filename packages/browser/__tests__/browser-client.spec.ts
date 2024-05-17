c4cf40be3186ce1abcb29dd7660bcce618bc2766
EvanWu <850123119@qq.com>
2024-05-17 15:38:39 +0800

    fix: 更改test

--- packages/browser/__tests__/browser-client.spec.ts
+++ packages/browser/__tests__/browser-client.spec.ts
@@ -3,12 +3,10 @@ import { urlConfig } from './config';
 
 describe('browserClient.ts', () => {
     const browserInstance = new BrowserClient({
-        url: urlConfig,
-        disabledFetch: true
+        url: urlConfig
     });
     it('browserClient should work', () => {
         // todo
         expect(browserInstance.report.url).toBe(urlConfig); // 校验report是否绑定
-        expect(browserInstance.options.disabledFetch).toBe(true); // 校验options是否绑定
     });
 });
